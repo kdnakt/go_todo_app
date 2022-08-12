de2b5eeb522f95c9a9981617b9c064f995a727db
kdnakt <a.kid.1985@gmail.com>
2022-08-12 11:06:47 +0900

    add missing comma

--- handler/response.go
+++ handler/response.go
@@ -8,11 +8,11 @@ import (
 )
 
 type ErrResponse struct {
-	Message string		`json:"message"`
-	Details []string	`json:"details,omitempty"`
+	Message string   `json:"message"`
+	Details []string `json:"details,omitempty"`
 }
 
-func RespondJSON(ctx context.Context, w http.ResponseWriter, body any status int) {
+func RespondJSON(ctx context.Context, w http.ResponseWriter, body any, status int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	bodyBytes, err := json.Marshal(body)
 	if err != nil {
