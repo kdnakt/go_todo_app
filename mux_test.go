d7eef4351c52798205c6152ddfa4163c4a56dbf4
kdnakt <a.kid.1985@gmail.com>
2022-08-20 11:40:04 +0900

    update mux test

--- mux_test.go
+++ mux_test.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"context"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/kdnakt/go_todo_app/config"
 )
 
 func TestNewMux(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest(http.MethodGet, "/health", nil)
-	sut := NewMux()
+	ctx := context.Background()
+	cfg, err := config.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	sut, cleanup, err := NewMux(ctx, cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cleanup()
 	sut.ServeHTTP(w, r)
 	resp := w.Result()
 	t.Cleanup(func() { _ = resp.Body.Close() })
