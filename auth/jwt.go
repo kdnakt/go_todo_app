91da9e530b557d6f9b96fbb5314652e8983c3d9c
kdnakt <a.kid.1985@gmail.com>
2022-08-28 21:09:38 +0900

    p.234: generate Store mock

--- auth/jwt.go
+++ auth/jwt.go
@@ -22,6 +22,7 @@ type JWTer struct {
 	Clocker               clock.Clocker
 }
 
+//go:generate go run github.com/matryer/moq -out moq_test.go . Store
 type Store interface {
 	Save(ctx context.Context, key string, userID entity.UserID) error
 	Load(ctc context.Context, key string) (entity.UserID, error)
