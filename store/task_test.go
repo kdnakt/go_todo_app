d74f402ce6d993dc277efa66c4eeaa8418d12bbc
kdnakt <a.kid.1985@gmail.com>
2022-09-19 10:03:46 +0900

    p.261: prepareUser for test

--- store/task_test.go
+++ store/task_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/kdnakt/go_todo_app/clock"
 	"github.com/kdnakt/go_todo_app/entity"
 	"github.com/kdnakt/go_todo_app/testutil"
+	"github.com/kdnakt/go_todo_app/testutil/fixture"
 )
 
 func TestRepository_ListTasks(t *testing.T) {
@@ -20,10 +21,10 @@ func TestRepository_ListTasks(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	wants := prepareTasks(ctx, t, tx)
+	wantUserID, wants := prepareTasks(ctx, t, tx)
 
 	sut := &Repository{}
-	gots, err := sut.ListTasks(ctx, tx)
+	gots, err := sut.ListTasks(ctx, tx, wantUserID)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
@@ -39,6 +40,7 @@ func TestRepository_AddTask(t *testing.T) {
 	c := clock.FixedClocker{}
 	var wantID int64 = 20
 	okTask := &entity.Task{
+		UserID:   entity.UserID(1),
 		Title:    "okk task",
 		Status:   "todo",
 		Created:  c.Now(),
@@ -51,9 +53,9 @@ func TestRepository_AddTask(t *testing.T) {
 	}
 	t.Cleanup(func() { db.Close() })
 	mock.ExpectExec(
-		`INSERT INTO task \(title, status, created, modified\)
-		VALUES \(\?, \?, \?, \?\)`,
-	).WithArgs(okTask.Title, okTask.Status, okTask.Created, okTask.Modified).
+		`INSERT INTO task \(user_id, title, status, created, modified\)
+		VALUES \(\?, \?, \?, \?, \?\)`,
+	).WithArgs(okTask.UserID, okTask.Title, okTask.Status, okTask.Created, okTask.Modified).
 		WillReturnResult(sqlmock.NewResult(wantID, 1))
 
 	xdb := sqlx.NewDb(db, "mysql")
@@ -63,39 +65,49 @@ func TestRepository_AddTask(t *testing.T) {
 	}
 }
 
-func prepareTasks(ctx context.Context, t *testing.T, con Execer) entity.Tasks {
+func prepareTasks(ctx context.Context, t *testing.T, con Execer) (entity.UserID, entity.Tasks) {
 	t.Helper()
 
 	if _, err := con.ExecContext(ctx, "DELETE FROM task;"); err != nil {
 		t.Logf("failed to initialize task: %v", err)
 	}
+
+	userID := prepareUser(ctx, t, con)
+	otherUserID := prepareUser(ctx, t, con)
 	c := clock.FixedClocker{}
 	wants := entity.Tasks{
 		{
+			UserID:   userID,
 			Title:    "want task 1",
 			Status:   "todo",
 			Created:  c.Now(),
 			Modified: c.Now(),
 		},
 		{
+			UserID:   userID,
 			Title:    "want task 2",
 			Status:   "todo",
 			Created:  c.Now(),
 			Modified: c.Now(),
 		},
+	}
+	tasks := entity.Tasks{
+		wants[0],
 		{
-			Title:    "want task 3",
+			UserID:   otherUserID,
+			Title:    "not want task",
 			Status:   "done",
 			Created:  c.Now(),
 			Modified: c.Now(),
 		},
+		wants[1],
 	}
 	result, err := con.ExecContext(ctx,
-		`INSERT INTO task (title, status, created, modified)
-			VALUES (?, ?, ?, ?), (?, ?, ?, ?), (?, ?, ?, ?);`,
-		wants[0].Title, wants[0].Status, wants[0].Created, wants[0].Modified,
-		wants[1].Title, wants[1].Status, wants[1].Created, wants[1].Modified,
-		wants[2].Title, wants[2].Status, wants[2].Created, wants[2].Modified,
+		`INSERT INTO task (user_id, title, status, created, modified)
+			VALUES (?, ?, ?, ?, ?), (?, ?, ?, ?, ?), (?, ?, ?, ?, ?);`,
+		tasks[0].UserID, tasks[0].Title, tasks[0].Status, tasks[0].Created, tasks[0].Modified,
+		tasks[1].UserID, tasks[1].Title, tasks[1].Status, tasks[1].Created, tasks[1].Modified,
+		tasks[2].UserID, tasks[2].Title, tasks[2].Status, tasks[2].Created, tasks[2].Modified,
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -104,8 +116,27 @@ func prepareTasks(ctx context.Context, t *testing.T, con Execer) entity.Tasks {
 	if err != nil {
 		t.Fatal(err)
 	}
-	wants[0].ID = entity.TaskID(id)
-	wants[1].ID = entity.TaskID(id + 1)
-	wants[2].ID = entity.TaskID(id + 2)
-	return wants
+	tasks[0].ID = entity.TaskID(id)
+	tasks[1].ID = entity.TaskID(id + 1)
+	tasks[2].ID = entity.TaskID(id + 2)
+	return userID, wants
+}
+
+func prepareUser(ctx context.Context, t *testing.T, db Execer) entity.UserID {
+	t.Helper()
+
+	u := fixture.User(nil)
+	result, err := db.ExecContext(ctx,
+		`INSERT INTO user (name, password, role, created, modified)
+		VALUES (?, ?, ?, ?, ?);`,
+		u.Name, u.Password, u.Role, u.Created, u.Modified,
+	)
+	if err != nil {
+		t.Fatalf("insert user: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		t.Fatalf("got user_id: %v", err)
+	}
+	return entity.UserID(id)
 }
